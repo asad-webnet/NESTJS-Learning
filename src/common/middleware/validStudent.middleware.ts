0c15a7588cdb24b25223905cf2dd4df33688e456
Muhammad Asad <theavden12@gmail.com>
2021-07-14 23:30:45 +0500

    Using middleware in our application

---
+++ src/common/middleware/validStudent.middleware.ts
@@ -0,0 +1,26 @@
+import { Injectable,NestMiddleware,HttpException } from "@nestjs/common";
+import { Request,Response,NextFunction } from "express";
+import { students } from "src/db";
+
+
+@Injectable()
+export class ValidStudentMiddleware implements NestMiddleware {
+
+    use(req:Request,res:Response,next:NextFunction) {
+        console.log("Middleware Called");
+        
+        const studentId  = req.params.studentId;
+        const studentExists = students.some(student => {
+            return student.id === studentId
+        })
+
+        if(!studentExists) {
+            throw new HttpException('Student not found',400)
+        }
+
+        next();        
+        
+
+    }
+
+}
--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { StudentController } from './student.controller';
 import { StudentService } from './student.service';
+import { ValidStudentMiddleware } from 'src/common/middleware/validStudent.middleware';
 
 @Module({
     controllers:[StudentController],
     providers: [StudentService],
     exports:[StudentService]
 })
-export class StudentModule {}
+export class StudentModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+
+        consumer.apply(ValidStudentMiddleware).forRoutes({
+            path: 'students/:studentId',
+            method: RequestMethod.GET
+        })
+        consumer.apply(ValidStudentMiddleware).forRoutes({
+            path: 'students/:studentId',
+            method: RequestMethod.PUT
+        })
+    }
+}
